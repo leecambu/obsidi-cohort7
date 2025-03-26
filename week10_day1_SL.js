08e01e7ed9d35cf41f2ec065589c2a38d478abc1
Lee <leecambu@gmail.com>
2025-03-25 23:24:31 -0400

    Added Week 10 Day 1 SL + Challenge

---
+++ week10_day1_SL.js
@@ -0,0 +1,46 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 9];
+
+// Using map() to create a new array with nested arrays. The second element will be the current number * 2
+const nestedArray = numbers.map(num => [num, num * 2]);
+console.log(nestedArray);
+
+
+// Using flat() to flatten the nested arrays
+const flatArray = numbers.map(num => [num, num * 2]).flat();
+console.log(flatArray);
+
+
+
+// Using flatMap() to create a new flattened array
+const flatMap = numbers.flatMap(num => [num, num * 2]);
+console.log(flatMap);
+
+
+//Print nested array 
+/*
+[
+  [ 1, 2 ],  [ 2, 4 ],
+  [ 3, 6 ],  [ 4, 8 ],
+  [ 5, 10 ], [ 6, 12 ],
+  [ 7, 14 ], [ 9, 18 ]
+]
+*/
+
+//Print flattened array
+/*
+[
+  1,  2, 2,  4, 3,  6,
+  4,  8, 5, 10, 6, 12,
+  7, 14, 9, 18
+]
+*/
+
+
+//Print flat mapped array
+/*
+[
+  1,  2, 2,  4, 3,  6,
+  4,  8, 5, 10, 6, 12,
+  7, 14, 9, 18
+]
+*/
